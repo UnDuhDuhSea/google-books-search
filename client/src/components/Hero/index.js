efa63d8efde7551051b91a906965a9f7c30db3c0
UnDuhDuhSea <tafimba@gmail.com>
2021-05-23 16:28:41 -0700

    Added Hero, Wrapper, Style content to hero

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -1,5 +1,12 @@
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <div className="jumbotron jumbotron-fluid">
+      <div className="container">
+        <h1 className="display-4">React Google Books Search</h1>
+        <p className="lead">Search for and save books</p>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
---
+++ client/src/components/Wrapper.js
@@ -0,0 +1,4 @@
+function Wrapper({ children }) {
+  return <div className="container mt-4">{children}</div>;
+}
+export default Wrapper;
