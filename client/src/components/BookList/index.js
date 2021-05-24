dfa707bf25542ee75603b60f8d6f1ef012dbed56
UnDuhDuhSea <tafimba@gmail.com>
2021-05-23 17:09:49 -0700

    Adding search form and section

--- client/src/components/BookList/index.js
+++ client/src/components/BookList/index.js
@@ -1,5 +1,14 @@
+import BookCard from "../BookCard";
+
 function BookList() {
-  return <div>BookList</div>;
+  return (
+    <div>
+      <h2 style={{ fontSize: "1.2rem" }}>Results</h2>
+      <BookCard />
+      <BookCard />
+      <BookCard />
+    </div>
+  );
 }
 
 export default BookList;
--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -1,24 +1,16 @@
+import Section from "../Section";
+
 function SearchForm() {
   return (
-    <div className="card text-white bg-secondary mb-3">
-      <div className="card-header">Book Search</div>
-      <div className="card-body">
-        <h2 className="card-title" style={{ fontSize: "1rem" }}>
-          Book Search
-        </h2>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-        <form className="d-flex flex-column">
-          <div className="form-group">
-            <label htmlFor="search">Book</label>
-            <input type="text" className="form-control" id="search" />
-          </div>
-          <button className="btn btn-primary ml-auto px-5">Search</button>
-        </form>
-      </div>
-    </div>
+    <Section title="Book Search">
+      <form className="d-flex flex-column">
+        <div className="form-group">
+          <label htmlFor="search">Book</label>
+          <input type="text" className="form-control" id="search" />
+        </div>
+        <button className="btn btn-primary ml-auto px-5">Search</button>
+      </form>
+    </Section>
   );
 }
 
